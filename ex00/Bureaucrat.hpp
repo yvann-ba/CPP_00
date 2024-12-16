1475088a804abcf2ad52cbe6d2c2c76a4f816b5d
yvann-ba <barbot.yvann@gmail.com>
2024-12-16 15:21:00 +0100

    finish ex00

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -2,14 +2,37 @@
 # define BUREAUCRAT_HPP
 
 #include <iostream>
+#include <stdexcept>
+#include <string>
 
 class Bureaucrat {
-    private:
-        std::string _name;
-        int _grade;
+
+    class GradeTooHighException : public std::exception {
     public:
-        std::string const & getName() const;
-        int const &getGrade() const;
+        virtual const char* what() const throw();
+    };
+    class GradeTooLowException : public std::exception {
+    public:
+        virtual const char* what() const throw();
+    };
+    
+private:
+    const std::string _name;
+    int _grade;
+
+public:
+    Bureaucrat(const std::string &name, int grade);
+    Bureaucrat(const Bureaucrat& other);
+    Bureaucrat &operator=(const Bureaucrat& other);
+    ~Bureaucrat();
+
+    std::string const & getName() const;
+    int const &getGrade() const;
+    void incrementGrade();
+    void decrementGrade();
+
+    friend std::ostream& operator<<(std::ostream& os, const Bureaucrat& b);
 
 };
+
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,5 +1,36 @@
+#include "Bureaucrat.hpp"
+
 int main () {
-    
+    try 
+    {
+        Bureaucrat poppy("poppy", 1);
+        std::cout << poppy << std::endl;
+
+        poppy.incrementGrade();
+        std::cout << poppy << std::endl;
+    }
+    catch(const std::exception& e) 
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    try
+    {
+        Bureaucrat invalidHigh("InvalidHigh", 0);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    try
+    {
+        Bureaucrat invalidLow("InvalidLow", 151);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
     return 0;
 }
