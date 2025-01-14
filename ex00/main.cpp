33751a3320a960c1a0ca8125b9d3fee2e872ca22
yvann-ba <barbot.yvann@gmail.com>
2025-01-14 11:54:57 +0100

    First push ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,34 @@
+#include "BitcoinExchange.hpp"
+#include <iostream>
+#include <vector>
+#include <list>
+
+int main(int ac, char *av[]) {
+    {
+        std::vector<int> vec;
+        vec.push_back(1);
+        vec.push_back(2);
+        vec.push_back(3);
+        try {
+            std::vector<int>::iterator it = easyfind(vec, 2);
+            std::cout << "Found: " << *it << std::endl; 
+        } catch (std::runtime_error &e) {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+
+    {
+        std::list<int> lst;
+        lst.push_back(10);
+        lst.push_back(20);
+        lst.push_back(30);
+        try {
+            std::list<int>::iterator it = easyfind(lst, 25);
+            std::cout << "Found: " << *it << std::endl;
+        } catch (std::runtime_error &e) {
+            std::cerr << e.what() << std::endl; 
+        }
+    }
+
+    return 0;
+}
